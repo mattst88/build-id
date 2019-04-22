8f13b4246acddd0cde1d34f81f4329dc22649d7d
Matt Turner <mattst88@gmail.com>
2019-04-22 15:03:40 -0700

    Add a program using dlopen

--- Makefile
+++ Makefile
@@ -23,13 +23,16 @@ LDLIBS = -ldl
 LDFLAGS = -Wl,--build-id=sha1
 GREP_SHA1 = egrep -o '\b[0-9a-f]{40}\b'
 
-all: build-id so-build-id
+all: build-id so-build-id dlopen-build-id
 
 build-id: test.o build-id.o
 	$(CC) $(LDFLAGS) $^ -o $@ $(LDLIBS)
 
 so-build-id: so-test.o libbuild-id.so
 	$(CC) $(LDFLAGS) $^ -o $@
+
+dlopen-build-id: dlopen-test.o
+	$(CC) $(LDFLAGS) $^ -o $@ $(LDLIBS)
 
 shared-build-id.o: build-id.c
 	$(CC) $(CPPFLAGS) $(CFLAGS) -fPIC -c $^ -o $@
@@ -43,16 +46,23 @@ build-id-test.expected: build-id
 so-build-id-test.expected: libbuild-id.so
 	file $< | $(GREP_SHA1) &> $@
 
+dlopen-build-id-test.expected: libbuild-id.so
+	file $< | $(GREP_SHA1) &> $@
+
 build-id-test.result: build-id
 	./$< | $(GREP_SHA1) &> $@
 
 so-build-id-test.result: so-build-id libbuild-id.so
 	LD_LIBRARY_PATH=. ./$< | $(GREP_SHA1) &> $@
 
-check: build-id-test.expected so-build-id-test.expected build-id-test.result so-build-id-test.result
+dlopen-build-id-test.result: dlopen-build-id libbuild-id.so
+	./$< | $(GREP_SHA1) &> $@
+
+check: build-id-test.expected so-build-id-test.expected dlopen-build-id-test.expected build-id-test.result so-build-id-test.result dlopen-build-id-test.result
 	cmp build-id-test.expected build-id-test.result
 	cmp so-build-id-test.expected so-build-id-test.result
+	cmp dlopen-build-id-test.expected dlopen-build-id-test.result
 	@echo PASS
 
 clean:
-	rm -f build-id so-build-id libbuild-id.so *.o *.result *.expected
+	rm -f build-id so-build-id dlopen-build-id libbuild-id.so *.o *.result *.expected
---
+++ dlopen-test.c
@@ -0,0 +1,70 @@
+/* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4: */
+/*
+ * Copyright © 2016 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "build-id.h"
+
+int
+main(int argc, char *argv[])
+{
+    const struct build_id_note *
+        (*build_id_find_nhdr_by_name)(const char *name);
+    const struct build_id_note *
+        (*build_id_find_nhdr_by_symbol)(const void *symbol);
+    ElfW(Word)
+        (*build_id_length)(const struct build_id_note *note);
+    const uint8_t *
+        (*build_id_data)(const struct build_id_note *note);
+
+    void *handle = dlopen("./libbuild-id.so", RTLD_LAZY);
+    if (!handle)
+        return -1;
+
+    build_id_find_nhdr_by_name = dlsym(handle, "build_id_find_nhdr_by_name");
+    build_id_find_nhdr_by_symbol = dlsym(handle, "build_id_find_nhdr_by_symbol");
+    build_id_length = dlsym(handle, "build_id_length");
+    build_id_data = dlsym(handle, "build_id_data");
+
+    if (dlerror())
+        return -1;
+
+    const struct build_id_note *note =
+        build_id_find_nhdr_by_name("./libbuild-id.so");
+    if (!note)
+        return -1;
+
+    ElfW(Word) len = build_id_length(note);
+
+    const uint8_t *build_id = build_id_data(note);
+
+    printf("Build ID: ");
+    for (ElfW(Word) i = 0; i < len; i++) {
+        printf("%02x", build_id[i]);
+    }
+    printf("\n");
+    dlclose(handle);
+    return 0;
+}
